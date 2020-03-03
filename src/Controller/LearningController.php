5124fd8593b608bff2e2551948e4a49f1afdacd0
emma <emma.wishart@live.com.au>
2020-03-03 11:41:23 +0100

    the first controller and entity added

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AboutMe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LearningController extends AbstractController
+{
+    /**
+     * @Route("/learning", name="learning")
+     */
+    public function index()
+    {
+        $user = new AboutMe();
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => $user->getName(),
+            'controller_text' => $user->getText()
+        ]);
+    }
+}
